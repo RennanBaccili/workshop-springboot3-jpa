20fb48ae82595e26be0a2b36d2060b2e7b4b4d70
Rennan Bacili <rennanbaccili@gmail.com>
2023-06-15 08:47:17 -0300

    H2 database,test profile, JPA

---
+++ src/main/java/com/educandowebRennan/course/entities/User.java
@@ -0,0 +1,102 @@
+package com.educandowebRennan.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+					
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {// implementação Serializable para converter objetos em cadeia de  bits
+
+	// id padrão do Serializable
+	private static final long serialVersionUID = 1L;
+	// atributos da classe User
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	// por causa do framework é obrigado construir um construtor vazio
+	public User() {
+		super();
+	}
+	
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		Id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	// hashcode para comparar iD
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
+}
