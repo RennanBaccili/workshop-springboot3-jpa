c5f948b983b010e995f453cb091098c77a9dc4d9
Rennan Bacili <rennanbaccili@gmail.com>
2023-06-18 11:39:43 -0300

    Many-to-Many association with JoinTable

--- workshop-springboot3-jpa/src/main/java/com/educandoweb/course/entities/Product.java
+++ workshop-springboot3-jpa/src/main/java/com/educandoweb/course/entities/Product.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 
 @Entity
@@ -29,7 +31,13 @@ public class Product implements Serializable{
 	// uso coleção Set pois ele representa um conjunto
 	// para garantir que eu não vou ter um produto com mais de uma ocorrencia da mesma categoria
 	// o mesmo produto não pode ter mais de uima categoria mais de uma vez
-	@Transient
+	
+	//chave estrangeira do product apois isso a chave da categoria
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	// instanciamos para garantir que a coleção não vai começar nula e sim vazia
 	// hashSet classe correspondente ao Set
